acb81363249678d00568451df93cedf0a42676da
andyWynyard <andrew.wynyard@gmail.com>
2019-04-02 11:48:23 +0200

    Made generic

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,7 +7,7 @@ import { withKnobs } from '@storybook/addon-knobs/react'
 import heading from './atoms/heading'
 import paragraph from './atoms/paragraph'
 
-const stories = storiesOf('Maverick|Atoms', module)
+const stories = storiesOf('Components|Atoms', module)
 
 stories.addDecorator(withKnobs)
 stories.addDecorator(checkA11y)
